67d4a2ef7f061f325953120ba6e941f47bd641cf
JayRodrig <mrjosemlrod@gmail.com>
2019-05-15 21:27:50 -0400

    added pgp modules to config file to make heroku work

--- services/db/config.js
+++ services/db/config.js
@@ -1,3 +1,6 @@
+//NPM MODULES
+const pgp = require("pg-promise")({});
+
 // GLOBAL VARIABLES
 const dbName = 'pantry_management';
 const dbAddr = pgp(process.env.DATABASE_URL || `postgres://localhost/${dbName}`);
