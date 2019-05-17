c5faadd98883593b5d2d9bbf7d5eab35d4180318
JayRodrig <mrjosemlrod@gmail.com>
2019-05-17 02:44:13 -0400

    added route to post a product

--- services/products.js
+++ services/products.js
@@ -3,6 +3,17 @@ const {getDbConn,} = require('./db/db');
 const {dbAddr,} = require('./db/config');
 
 // PRODUCT SERVICE FUNCTIONS
+const postProduct = (
+        product_name, product_url, product_image, product_original_weight, product_gram_weight, product_price, product_owner
+    ) => getDbConn(dbAddr).none(
+    `
+        INSERT INTO products 
+            (product_name, product_url, product_image, product_original_weight, product_gram_weight, product_price, product_owner)
+        VALUES 
+            ($[product_name], $[product_url], $[product_image], $[product_original_weight], $[product_gram_weight], $[product_price], $[product_owner])
+    `, {product_name, product_url, product_image, product_original_weight, product_gram_weight, product_price, product_owner}
+);
+
 const getProductByID = id => getDbConn(dbAddr).any(
     `
         SELECT * 
@@ -12,5 +23,6 @@ const getProductByID = id => getDbConn(dbAddr).any(
 );
 
 module.exports = {
+    postProduct,
     getProductByID,
 };
