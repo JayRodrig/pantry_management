ec48b70ecee35b83ed3444c38bb91b9627fca475
Erika Perez <erikaperez@Erikas-Air.fios-router.home>
2019-05-17 22:35:55 -0400

    added getInhgredientByName route in ingredients route

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -21,12 +21,30 @@ const getIngredientByID = (request, response) => {
         });
 };
 
+const getIngredientByName = (request, response) => {
+    const {name,} = request.params;
+    IngredientServices.getIngredientByName(name)
+        .then(data => {
+            response.status(200).json({
+                'msg': `Successfully retrieved user data.`,
+                data,
+            });
+        })
+        .catch(e => {
+            response.status(400).json({
+                'msg': `Something went wrong.`,
+                e,
+            });
+        });
+};
+
 
 
 const getIngredientRouter = _ => {
     const IngredientRouter = express.Router();
 
     IngredientRouter.get('/:id', getIngredientByID);
+    IngredientRouter.get('/name/:name', getIngredientByName);
 
     return IngredientRouter;
 };
--- services/ingredient.js
+++ services/ingredient.js
@@ -3,6 +3,8 @@ const {getDbConn,} = require('./db/db');
 const {dbAddr,} = require('./db/config');
 
 // PRODUCT SERVICE FUNCTIONS
+
+//GET INGREDIENT BY ID
 const getIngredientByID = id => getDbConn(dbAddr).any(
     `
         SELECT * 
@@ -11,6 +13,17 @@ const getIngredientByID = id => getDbConn(dbAddr).any(
     `, {id,}
 );
 
+//GET INGREDIENT BY NAME
+const getIngredientByName = name => getDbConn(dbAddr).any(
+    `
+        SELECT * 
+            FROM ingredients
+        WHERE ingredients.ingredient_name = $[name]
+    `, {name,}
+);
+
+
 module.exports = {
     getIngredientByID,
+    getIngredientByName,
 };
