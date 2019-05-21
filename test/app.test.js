45a4d6fb4a0bcbeba62873dac8e90ef6724f3660
Erika Perez <erikaperez@Erikas-Air.fios-router.home>
2019-05-21 00:11:12 -0400

    corrected number mock calls length in app.test

--- test/app.test.js
+++ test/app.test.js
@@ -37,6 +37,6 @@ test('getApp tests', done => {
 
     expect(app).toEqual(mockApp);
     expect(mockUse.mock.calls[0][0]).toBe('test');
-    expect(mockUse.mock.calls.length).toEqual(6);
+    expect(mockUse.mock.calls.length).toEqual(7);
     done();
 });
