cae32729e6df20c6315d2695194ed5e4f5b5d136
JayRodrig <mrjosemlrod@gmail.com>
2019-05-22 01:54:51 -0400

    wrote all initial tests for ./services/users.js

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -22,3 +22,59 @@ test('postUser will call getDbConnect.oneOrNone', done => {
             done();
         });
 });
+
+test('getUserById will call getDbConnect.one', done => {
+    const mockOne = jest.fn(() => Promise.resolve());
+    getDbConn.mockImplementation(() => {
+        return {
+            one: mockOne,
+        }
+    });
+    getUserByID()
+        .then(_ => {
+            expect(mockOne.mock.calls.length).toBe(1);
+            done();
+        });
+});
+
+test('getUserByEmail will call getDbConnect.one', done => {
+    const mockOne = jest.fn(() => Promise.resolve());
+    getDbConn.mockImplementation(() => {
+        return {
+            one: mockOne,
+        }
+    });
+    getUserByEmail()
+        .then(_ => {
+            expect(mockOne.mock.calls.length).toBe(1);
+            done();
+        });
+});
+
+test('updateUser will call getDbConnect.oneOrNone', done => {
+    const mockOneOrNone = jest.fn(() => Promise.resolve());
+    getDbConn.mockImplementation(() => {
+        return {
+            oneOrNone: mockOneOrNone,
+        }
+    });
+    updateUser()
+        .then(_ => {
+            expect(mockOneOrNone.mock.calls.length).toBe(1);
+            done();
+        });
+});
+
+test('deleteUser will call getDbConnect.oneOrNone', done => {
+    const mockOneOrNone = jest.fn(() => Promise.resolve());
+    getDbConn.mockImplementation(() => {
+        return {
+            oneOrNone: mockOneOrNone,
+        }
+    });
+    deleteUser()
+        .then(_ => {
+            expect(mockOneOrNone.mock.calls.length).toBe(1);
+            done();
+        });
+});
