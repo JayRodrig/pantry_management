a330ab05dc586ae839d337c819fa8e0bdc0afc50
JayRodrig <mrjosemlrod@gmail.com>
2019-05-25 15:33:33 -0400

    added route recipeByPantry, will return recipes according to the users virtual pantry

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,11 @@
 
 // NPM MODULES
 const express = require('express');
+const axios = require('axios');
 
 // LOCAL MODULES
 const UserServices = require('../services/users');
+const CurrentPantryServices = require('../services/currentPantry');
 
 const createUser = (request, response) => {
     const {name, username, email, dob, phone_number, diet_preference, food_limitations, food_alergies,} = request.body;
@@ -96,12 +98,50 @@ const deleteUser = (request, response) => {
         });
 };
 
+const recipesByPantry = (request, response) => {
+    const {email,} = request.params;
+    UserServices.getUserByEmail(email)
+        .then(async data => {
+            const {user_id,} = data;
+            const pantryItems = await CurrentPantryServices.getPantryItemsOfUser(user_id);
+            let baseAPIURL = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=`;
+            for (let i = 0; i < pantryItems.length; i++) {
+                const ingredientStr = `${pantryItems[i].weight_left} grams of ${pantryItems[i].product_name}`
+                if (i < pantryItems.length - 1) {
+                    baseAPIURL += `${ingredientStr}%2C`;
+                } else {
+                    baseAPIURL += ingredientStr;
+                };
+            };
+            const recipes = await axios({
+                method: 'get',
+                url: baseAPIURL,
+                headers: {
+                    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+                    'X-RapidAPI-Key': '152e0cdc26msh98d504390075f2dp1eeb72jsn4ab228d3c452',
+                },
+            });
+            response.status(200).json({
+                'msg': `Successfully retrieved recipes.`,
+                pantryItems,
+                recipes: recipes.data,
+            }); 
+        })
+        .catch(e => {
+            response.status(400).json({
+                'msg': `Something went wrong`,
+                e,
+            });
+        });
+};
+
 const getUserRouter = _ => {
     const UserRouter = express.Router();
 
     UserRouter.post('/', createUser);
     UserRouter.get('/id/:id', getUserByID);
     UserRouter.get('/email/:email', getUserByEmail);
+    UserRouter.get('/recipebypantry/:email', recipesByPantry)
     UserRouter.put('/:id', updateUser);
     UserRouter.delete('/:id', deleteUser);
 
