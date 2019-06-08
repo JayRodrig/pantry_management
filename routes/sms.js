6cbbd57dbd6a8cc3548acf23296693e2ab5d6759
Heriberto Uroza <heribertouroza@pursuit.org>
2019-06-08 10:36:10 -0400

    updated travis for twilio

---
+++ routes/sms.js
@@ -0,0 +1,51 @@
+// NPM MODULES
+const express = require('express');
+
+// LOCAL MODULES
+const { upcomingMealsIngList, } = require('../services/shopping_list');
+const config = require('../services/twilio/config.js')
+const accountSid = config.accountSid
+const authToken = config.authToken
+const client = require('twilio')(accountSid, authToken);
+
+//SEND SMS
+const sendSMS = async ( request, response ) => {
+    const { user_id, phone_number} = request.params;
+    const ingredientsList = await upcomingMealsIngList(user_id);
+
+    const values = Object.values(ingredientsList)
+    let textMessageBody = 'At least... ';
+    values.forEach(e => {
+        textMessageBody += `At least ${e.needed_weight} grams of ${e.product_name} is needed. `
+    })
+
+    client.messages
+        .create({
+            body: textMessageBody,
+            from: config.number,
+            to: phone_number
+        })
+        .then(message => {
+            console.log(message)
+            response.json({
+                message: 'Text Message Has Been Sent',
+                status: message.status
+            })
+        })
+        .catch(err => {
+            console.log(err.toString())
+        })
+}
+
+
+const sendSMSRouter = _ => {
+    const sendSMSRouter = express.Router();
+
+    sendSMSRouter.get('/:user_id/:phone_number', sendSMS);
+    
+    return sendSMSRouter;
+};
+
+module.exports = {
+    sendSMSRouter,
+};
---
+++ services/twilio/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "number" : process.env.TW_NUMBER ,
+    "accountSid": process.env.TW_SID,
+    "authToken": process.env.TW_TOKEN
+}
